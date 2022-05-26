985c0cdc0b4cec7da1d3673c2becad2af1875614
pdf-0 <pdf-0@github.com>
2022-05-26 10:19:50 +0200

    BB en l_etat de l_oral pour consulatation ulterieure

--- BandwidthBroker/bb_for_eclipse/src/ThreadTCP.java
+++ BandwidthBroker/bb_for_eclipse/src/ThreadTCP.java
@@ -14,56 +14,74 @@ public class ThreadTCP extends Thread {
  
     public void run() {
         try {
+        	boolean first_time=true;
             InputStream input = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
  
             OutputStream output = socket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
  
- //TODO finalement maintenir la conversation ouverte en permanence?
+ //TODO finalement maintenir la conversation ouverte en permanence ou pas?
             String text;
-            Boolean answered = false;
  
             do {
                 //texte envoyé par le client lu par le serveur
+            	System.out.println("BEFORE readline");
                 text = reader.readLine();
-                String[] tab = text.split(",");
-                //TODO TODO
-                //TODO exclusion mutuelle des threads
-                //texte renvoyé au client
-                //InetAddress source, InetAddress receiver, int portSource, int portRecei, double demand
-                if (tab.length==5){
-                	InetAddress source = InetAddress.getByName(tab[0]);
-                	InetAddress receiver = InetAddress.getByName(tab[1]);
-                	int portsrc = Integer.valueOf(tab[2]);
-                	int portrcv = Integer.valueOf(tab[3]);//pas d'utilité à toujours garder ces deux ports qui vaudront toujours 5060, sauf pour détecter un éventuel détournement
-                	Double demand=Double.parseDouble(tab[4]);
-                    boolean test_rs = BB.checkRessourceUtilization(demand);
-                    String src_dest = "-"+tab[0]+"-"+tab[1];
-                    int connexstate = BB.addRessourcesConnexions(source, receiver, portsrc, portrcv, demand);
-                    
-                    if (0%2==connexstate && test_rs){
-                        writer.println("OK"+src_dest);
-                        BB.addConnexion(new Connexion(source, receiver, portsrc, portrcv, demand, BB.GetId_branch()), demand);
-                    }else if (1==connexstate){
-                        writer.println("NOK"+src_dest);
+                if (text==null) {
+                	if (first_time) {
+	                	System.out.println("Fin de la connexion avec un client TCP (probablement un proxy SIP)");
+	                    ThreadTCP.currentThread().interrupt();
+	                    ThreadTCP.currentThread().stop();
+	                    first_time=false;
                     }
-                    //answered = true;
-                }else if (tab.length==2){//ip et port
-                    if (BB.RemoveRessourcesConnexions(InetAddress.getByName(tab[0]), Integer.valueOf(tab[1]))){
-                        writer.println("OK-"+tab[0]+"-closure");
-                    }else{
-                        writer.println("NOK-"+tab[0]+"-closure");
-                    }
-                    answered = true;
+                }else {
+	                String[] tab = text.split("-");
+	            	System.out.println(text);
+	                //texte renvoyé au client
+	                //InetAddress source, InetAddress receiver, int portSource, int portRecei, double demand
+	                if (tab.length==5){
+	                	
+	                	InetAddress source = InetAddress.getByName(tab[0]);
+	                	InetAddress receiver = InetAddress.getByName(tab[1]);
+	                	int portsrc = Integer.valueOf(tab[2]);
+	                	int portrcv = Integer.valueOf(tab[3]);//pas d'utilité à toujours garder ces deux ports qui vaudront toujours 5060, sauf pour détecter un éventuel détournement
+	                	Double demand=Double.parseDouble(tab[4]);
+	                    boolean test_rs = BB.checkRessourceUtilization(demand);
+	                    
+	                    String src_dest = "-"+tab[0]+"-"+tab[1];
+	                    int connexstate = BB.addRessourcesConnexions(source, receiver, portsrc, portrcv, demand); //Manipulation d'une connexion déjà existante
+	                    
+	                    if (0==connexstate && test_rs){
+	                    	BB.addConnexion(new Connexion(source, receiver, portsrc, portrcv, demand, BB.GetId_branch()), demand);
+	                    	BB.printConnexionTable();
+	                        writer.println("OK"+src_dest);
+	                        System.out.println("---OK envoyé---");
+	                    }else if (1==connexstate || !test_rs){
+	                    //if (true) {//à des fins de test uniquement TODO 
+	                        writer.println("NOK"+src_dest);
+	                        System.out.println("Une communication a été refusée.");
+	                    }else if (2==connexstate) {
+	                    	System.out.println("Warning - Une communication déjà en cours a été redemandée.");
+	                        writer.println("OK"+src_dest);
+	                        System.out.println("-----(OK re-envoyé)");
+	                    }else {
+	                    	System.out.println("WARNING --- APPARITION D'UNE ERREUR EN THEORIE IMPOSSIBLE");
+	                    }
+	                }else if (tab.length==2){//ip et port
+	                    if (BB.RemoveRessourcesConnexions(InetAddress.getByName(tab[0]), 5060)){
+	                    	BB.printConnexionTable();
+	                        writer.println("OK-"+tab[0]+"-closure");
+	                    }else{
+	                        writer.println("NOK-"+tab[0]+"-closure");
+	                    }
+	                }
+	                else{
+	                    System.out.println("WARNING : message reçu du proxy SIP de format invalide");
+	                }
                 }
-                else{
-                    System.out.println("WARNING : message reçu du proxy SIP de format invalide");
-                }
+            } while (true);
  
-            } while (!answered);//TODO modifier
- 
-            ThreadTCP.currentThread().interrupt();
 
         } catch (Exception ex) {
             System.out.println("Exception (thread serveur): " + ex.getMessage());
--- BandwidthBroker/bb_for_eclipse/src/bandwidthbroker.java
+++ BandwidthBroker/bb_for_eclipse/src/bandwidthbroker.java
@@ -1,8 +1,8 @@
 import java.lang.management.ThreadInfo;
 import java.net.*;
 import java.util.*;
-import java.io.*;
 import com.jcraft.jsch.*;
+import java.io.*;
 
 
 
@@ -17,64 +17,86 @@ public class bandwidthbroker{
 
     // Tableau récapitulatif des connexions établies
     private ArrayList<Connexion> t_connexion = new ArrayList<Connexion>();
-    private int listening_port = 12121;//se mettre d'accord avec le SIP avant tout changement
-
+    private int listening_port = 12121;//note: le proxy SIP utilise le même
+    private String proxy_SIP_IP = "193.168.1.2";
+    
     private String CE_A = "193.168.1.254";
     private String CE_B = "193.168.2.254";
-    private String portSIP="5060";//attention tout ça est statique et n supporte pas de modifications de la topographie du réseau
-
+    private String portSIP="5060";//attention tout ça est statique et ne supporte pas de modifications de la topographie du réseau
+    private String eth_used = "eth1";//en cas de changement penser à netoyer les qdiscs sur l'ancienne
     private int id_branch;
 
     private static String BB_IP = "193.168.1.1";
 
 
     public bandwidthbroker(){
-
-    	//initialisation des iptables mangle pour MPLS
-    	String mangle_init= "iptables -t mangle -A FORWARD -p udp --dport "+portSIP+" -j DSCP --set-dscp-class EF;iptables -t mangle -A OUTPUT -p udp --dport "+portSIP+" -j DSCP --set-dscp-class EF;";
-
     	
         //initialisation des tc avec les SLAs
-        String TC_A_init = "tc qdisc del dev eth0 root;tc qdisc add dev eth0 root handle 1: htb default 20;tc class add dev eth0 parent 1: classid 1:1 htb rate 1000kbit ceil 1100kbit;tc class add dev eth0 parent 1: classid 1:2 htb rate 2000kbit ceil 2100kbit;tc filter add dev eth0 parent 1: protocol ip prio 1 handle 20 fw flowid 1:2";
-        //1: voix, 2:parasite ->handle 20 va en parasite?
-        String TC_B_init = "tc qdisc del dev eth0 root;tc qdisc add dev eth0 root handle 1: htb default 20;tc class add dev eth0 parent 1: classid 1:1 htb rate 2000kbit ceil 2100kbit;tc class add dev eth0 parent 1: classid 1:2 htb rate 3000kbit ceil 3100kbit;tc filter add dev eth0 parent 1: protocol ip prio 1 handle 20 fw flowid 1:2";
-              
+        String TC_A_init = "tc qdisc del dev "+eth_used+" root;tc qdisc add dev "+eth_used+" root handle 1: htb default 2;tc class add dev "+eth_used+" parent 1: classid 1:1 htb rate 1000kbit ceil 1100kbit;tc class add dev "+eth_used+" parent 1: classid 1:2 htb rate 2000kbit ceil 2100kbit;tc filter add dev "+eth_used+" parent 1: protocol ip prio 1 fw flowid 1:2";
+        //1: voix, 2:parasite ->handle 2 va en 1:2 parasite
+        String TC_B_init = "tc qdisc del dev "+eth_used+" root;tc qdisc add dev "+eth_used+" root handle 1: htb default 2;tc class add dev "+eth_used+" parent 1: classid 1:1 htb rate 2000kbit ceil 2100kbit;tc class add dev "+eth_used+" parent 1: classid 1:2 htb rate 3000kbit ceil 3100kbit;tc filter add dev "+eth_used+" parent 1: protocol ip prio 1 fw flowid 1:2";
+
         
-        try {
- 			askSSH(this.CE_A, mangle_init);
- 	        askSSH(this.CE_B, mangle_init);//TODO décommenter
- 		} catch (Exception e) {
- 			e.printStackTrace();
- 			System.out.println("Erreur à l'initialisation des iptables via ssh.");
- 		}
+        init_iptables();
+
         try {
 			askSSH(this.CE_A, TC_A_init);
-	        askSSH(this.CE_B, TC_B_init);//TODO décommenter
+	        askSSH(this.CE_B, TC_B_init);
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Erreur à l'initialisation des qdiscs via ssh.");
 		}
         //pour plus de clients possible de faire un for avec un dictionnaire associant host et commandes
 
-        this.id_branch = 1;
-
-        //this.listenTCP();//TODO décommenter
-
+        this.id_branch = 5;
+        this.listenTCP();
 
     }
 
+    
+    private void init_iptables() {
+
+    	//initialisation des iptables mangle pour MPLS
+    	/*String mangle_check_F = "iptables -t mangle -C FORWARD -p udp --dport "+portSIP+" -j DSCP --set-dscp-class EF;echo $?";
+    	String mangle_check_O = "iptables -t mangle -A OUTPUT -p udp --dport "+portSIP+" -j DSCP --set-dscp-class EF;echo $?";
+    	String mangle_init_F = "iptables -t mangle -A FORWARD -p udp --dport "+portSIP+" -j DSCP --set-dscp-class EF;";
+    	String mangle_init_O = "iptables -t mangle -A OUTPUT -p udp --dport "+portSIP+" -j DSCP --set-dscp-class EF;";
+    	*/
+    	String mangle_init_EF = "iptables -t mangle -A FORWARD -p udp --dport "+portSIP+" -j DSCP --set-dscp-class EF;iptables -t mangle -A OUTPUT -p udp --dport "+portSIP+" -j DSCP --set-dscp-class EF;";
+    	String mangle_proxySIP = "iptables -t mangle -A FORWARD -p udp -d "+proxy_SIP_IP+" -j DSCP --set-dscp-class CS4;iptables -t mangle -A OUTPUT -p udp -d "+proxy_SIP_IP+" -j DSCP --set-dscp-class CS4;iptables -t mangle -A FORWARD -p udp -s "+proxy_SIP_IP+" -j DSCP --set-dscp-class CS4;iptables -t mangle -A OUTPUT -p udp -s "+proxy_SIP_IP+" -j DSCP --set-dscp-class CS4;";
+    	//peu importe si on utilise sport ou dport, ce sont les mêmes
+    	//pas obligatoire de préciser les ports pour les mangle_proxySIP
+        try {/*
+            //ici un début de strings pour vérification de la non-existence des règles dans la table mangle pour éviter une redondance
+ 			askSSH(this.CE_A, mangle_init_F);
+ 	        askSSH(this.CE_B, mangle_init_O);
+ 			askSSH(this.CE_A, mangle_init_F);
+ 	        askSSH(this.CE_B, mangle_init_O);*/
+        	//!!!!!!!!!!!!!CRUCIAL de passer d'abord les règles proxySIP pour qu'elles soient prioritaires!!!
+ 			//askSSH(this.CE_A, mangle_proxySIP+mangle_init_EF);
+ 	        //askSSH(this.CE_B, mangle_proxySIP+mangle_init_EF);
+        	//!!!en l'état actuel pas de supression ou de check des iptables avant d'en rajouter, donc décommenter ces deux lignes rajoutera juste des règles redondantes!
+ 		} catch (Exception e) {
+ 			e.printStackTrace();
+ 			System.out.println("Erreur à l'initialisation des iptables via ssh.");
+ 		}
+        System.out.println("--Initialisation des iptables mangle sautée car déjà rentrées précédemment.--");
+    }
+    
+    
     public int GetId_branch() {
     	return this.id_branch;
     }
     
+    
     //communication ssh
     //pour faire passer plusieurs commandes, passer en arg "cmd1 ; cmd2" !!
-    private static void askSSH(String host, String commande) throws Exception {
+    private static String askSSH(String host, String commande) throws Exception {
         //possible de modifier pour faire passer username et password en argument pour plus de généralité sur le déploiement
-          
+    		String responseString = "error";
           Session session = null;
           ChannelExec channel = null;
-          String password="blanshneij";//faux bien sûr, à modifier
+          String password="blanshneij";//TODO faux bien sûr, à modifier pour utilisation réelle
           //utiliser un property file pour la sécurité?
           
           try {
@@ -93,13 +115,14 @@ public class bandwidthbroker{
                   Thread.sleep(100);
               }
               
-              String responseString = new String(responseStream.toByteArray());
+              responseString = new String(responseStream.toByteArray());
               System.out.println(responseString);
           } finally {
               if (session != null || channel!=null) {
                   session.disconnect();
               }
           }
+          return responseString;
       }
 
 
@@ -125,7 +148,9 @@ public class bandwidthbroker{
         //Mise à jour des ressources
         for(int i=0;i<SLA_u.length;i++){
             SLA_u[i]+=demand;
-        }	
+        }
+        
+        System.out.println("...connexion ajoutée! ("+connOK.getSource().getHostAddress()+" to "+connOK.getReceiver().getHostAddress()+")");
     }
 
     // Si connexion autorisée / fermée mise à jour de la table des connexions et utilisation des ressources
@@ -136,17 +161,20 @@ public class bandwidthbroker{
         }
         else {
             int index = 0;
-            while (index <= t_connexion.size() && already_exists==0){
-                //possible de remplacer tout ça par une vérification de l'id_branch?
-                if(t_connexion.get(index).getSource().equals(source)){
-                    already_exists++;
-                }
-                if(t_connexion.get(index).getReceiver().equals(receiver)){
-                    already_exists++;
-                }
-                else{
-                    index++;
-                }           
+            if (t_connexion.size()>0) {
+            	
+	            while (index < t_connexion.size() && already_exists==0){
+	                //possible de remplacer tout ça par une vérification de l'id_branch?
+	                if(t_connexion.get(index).getSource().equals(source)){
+	                    already_exists++;
+	                }
+	                if(t_connexion.get(index).getReceiver().equals(receiver)){
+	                    already_exists++;
+	                }
+	                else{
+	                    index++;
+	                }           
+	            }
             }
             if (already_exists==0) {
                 try {
@@ -156,7 +184,7 @@ public class bandwidthbroker{
     				System.out.println("Echec de la mise à jour des routeurs via ssh.");
     			}
 
-	        System.out.println("La connexion va être ajoutée...");
+	        System.out.println("La connexion "+source.getHostAddress()+" to "+receiver.getHostAddress()+" va être ajoutée...");
 	        }
 	    }
 	    return already_exists;//à utiliser par la communication TCP
@@ -170,50 +198,53 @@ public class bandwidthbroker{
         int id = 0;
         boolean trouve = false;
         double bpaenlever = 0.0;
-        while (index <= t_connexion.size() && trouve==false){
-            //possible de remplacer tout ça par une vérification de l'id_branch?
-            if(t_connexion.get(index).getSource().equals(source) && t_connexion.get(index).getPortSource()==portSource){
-                id=t_connexion.get(index).getId_branch();
-                bpaenlever = t_connexion.get(index).getBandWidth();
-                t_connexion.remove(index);
-                trouve=true;
-            }
-            else{
-                index++;
-            }           
-        }
-        
-        try {
-			qdisc_EF_branch_update(false,(int)(1000*(float)bpaenlever), source, portSource,id);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Echec de la mise à jour des routeurs via ssh.");
-		}
-
-        //Mise à jour des ressources
-        for(int i=0;i<SLA_u.length;i++){
-            SLA_u[i]-=bpaenlever;
-        }
-
-
-        System.out.println("La connexion a été supprimée.");
-
+ 
+	        while (index < t_connexion.size() && trouve==false){
+	            //possible de remplacer tout ça par une vérification de l'id_branch?
+	            if(t_connexion.get(index).getSource().equals(source) && t_connexion.get(index).getPortSource()==portSource){
+	                id=t_connexion.get(index).getId_branch();
+	                bpaenlever = t_connexion.get(index).getBandWidth();
+	                t_connexion.remove(index);
+	                trouve=true;
+	            }
+	            else{
+	                index++;
+	            }           
+	        }
+	        if (trouve) {
+		        try {
+					qdisc_EF_branch_update(false,(int)(1000*(float)bpaenlever), source, portSource,id);
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.out.println("Echec de la mise à jour des routeurs via ssh.");
+				}
+		        //Mise à jour des ressources
+		        for(int i=0;i<SLA_u.length;i++){
+		            SLA_u[i]-=bpaenlever;
+		        }
+		        System.out.println("La connexion a été supprimée.");
+		        
+		     }else {
+		        	System.out.println("--(suppression d'une communication non enregistrée demandée)--"); 
+		        	retour = true;//en l'état on renverra toujours OK-closure et jamais NOK-closure
+		     }
+ 
         return retour;//à utiliser par la communication TCP
     }
     
-
+//Pierre
     private void qdisc_EF_branch_update(boolean creatrue_removalse, int debit_asked, InetAddress ipsource, int portsource, int indice) throws Exception{
        String update_tc = "";
        String indc=Integer.toString(indice+10);//on incrémente de 10 pour être sûr qu'il n'y ait pas d'interférences avec 1:2 qui est le best effort, vérifier le fonctionnement de tc
        String addr_src=ipsource.getHostAddress();
        if (creatrue_removalse){
             String dba=Integer.toString(debit_asked);        
-            update_tc = "tc class add dev eth0 parent 1:1 classid 1:"+indc+" htb rate "+dba+"kbit ceil "+dba+"kbit";
-            update_tc = update_tc+";tc filter add dev eth0 parent 1:1 protocol ip prio 1 u32 match ip src "+addr_src+"/32 match ip dport "+Integer.toString(portsource)+" 0xffff flowid 1:"+indc;
+            update_tc = "tc class add dev "+eth_used+" parent 1:1 classid 1:"+indc+" htb rate "+dba+"kbit ceil "+dba+"kbit";
+            update_tc = update_tc+";tc filter add dev "+eth_used+" parent 1:1 protocol ip prio 1 u32 match ip src "+addr_src+"/32 match ip dport "+Integer.toString(portsource)+" 0xffff flowid 1:"+indc;
        
         }else{//note: il est obligatoire de del tous les filters et classes filles avant de supprimer une classe
-            update_tc = "tc filter del dev eth0 parent 1:1 protocol ip prio 1 u32 match ip src "+addr_src+"/32 match ip dport "+Integer.toString(portsource)+" 0xffff flowid 1:"+indc;
-            update_tc = update_tc+";tc class del dev eth0 classid 1:"+indc;
+            update_tc = "tc filter del dev "+eth_used+" parent 1:1 protocol ip prio 1 u32 match ip src "+addr_src+"/32 match ip dport "+Integer.toString(portsource)+" 0xffff flowid 1:"+indc;
+            update_tc = update_tc+";tc class del dev "+eth_used+" classid 1:"+indc;
         }
 
 		askSSH(this.CE_A, update_tc);
@@ -225,7 +256,6 @@ public class bandwidthbroker{
 
 
     // Connexion TCP avec proxy SIP
-    //TODO finish
     // Pierre
     private void listenTCP(){
         try (ServerSocket servSock = new ServerSocket(listening_port)) {
@@ -234,8 +264,7 @@ public class bandwidthbroker{
 
             while (true) {
                 Socket socket = servSock.accept();
-                System.out.println("Connexion d'un client TCP");
-
+                System.out.println("Connexion d'un client TCP (probablement un proxy SIP)");
                 new ThreadTCP(socket, this).start();//surtout pas run, start fait le parallelisme
             }
 
@@ -256,7 +285,7 @@ public class bandwidthbroker{
 
     // Affichage du tableau des connexions et de l'utilisation des ressources en %
     // Quentin
-    private void printConnexionTable(){
+    protected void printConnexionTable(){
 
         //Afficher la table des connexion;
         System.out.println("SOURCE       RECEIVER       PORTSOURCE    PORTRECE    BANDWIDTH(Mbps)       ");
@@ -273,9 +302,9 @@ public class bandwidthbroker{
 
 
     public static void main(String[] args) {
-
-        bandwidthbroker b = new bandwidthbroker();
-        String[] reserv = b.parseString("192.168.1.1,192.168.1.2,6666,7777,0.5");
+    	
+    	bandwidthbroker b = new bandwidthbroker();
+        /*String[] reserv = b.parseString("192.168.1.1,192.168.1.2,6666,7777,0.5");
         System.out.println(reserv[0] + " " + reserv[1] + " " + reserv[2] + " " + reserv[3] + " " + reserv[4]);
         boolean test = b.checkRessourceUtilization(Double.parseDouble(reserv[4]));
         if(test==true){
@@ -288,7 +317,7 @@ public class bandwidthbroker{
         }
         else{
             System.out.println("Le test a échoué.");
-        }
+        }*/
 
         //Cas avec une demande trop grande
         /*test = b.checkRessourceUtilization(2.0);
